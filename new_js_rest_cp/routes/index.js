508902450e0e2a00ed0328459efccfd651b4fe38
Suryaansh-Jain <91973466+Suryaansh-Jain@users.noreply.github.com>
2022-06-25 22:33:00 +0530

    Update index.js

--- new_js_rest_cp/routes/index.js
+++ new_js_rest_cp/routes/index.js
@@ -12,25 +12,50 @@ import {
     getHospitalWithVaccine,
     ReportCovidCase,
     numberCasesToday,
-    ReportCovidNegative
+    ReportCovidNegative,
+    covidDataCummilative,
+    createAdmin,
+    createHospital,
+    createUser,
+    createStaff,
+    addVacccine,
+    totalDailyData,
+    totalCovidDataCummilative,
+    ReportDeath,
+    updateCummilativeData
 } from "../controllers/Products.js";
 const router = express.Router();
-router.get('/GetAllUser', getAllUser);
-router.get('/GetAllPositive', getAllPositiveUsers);
-router.get('/GetUserById/:aadharId', getUserById);
 
-router.get('/GetAdminByID/:id', getAdminById);
-router.get('/GetAllAdmin', getAllAdmin);
+router.get('/User', getAllUser);
+router.get('/User/Positive', getAllPositiveUsers);
+router.get('/User/:aadharId', getUserById);
 
-router.get('/getAllHospitals', getAllHospital);
-router.get('/getHospitalWithVaccine', getHospitalWithVaccine);
-router.get('/getHospitalById/:id', getHospitalById);
+router.get('/Admin/:id', getAdminById);
+router.get('/Admin', getAllAdmin);
 
-router.get('/getAllStaff', getAllStaff);
-router.get('/getStaffById', getStaffById);
+router.get('/Hospital', getAllHospital);
+router.get('/Hospital/WithVaccine', getHospitalWithVaccine);
+router.get('/Hospital/:id', getHospitalById);
 
-router.patch('/ReportCase/:aadharId', ReportCovidCase);
-router.patch('/ReportNegative/:aadharId', ReportCovidNegative);
+router.get('/Staff', getAllStaff);
+router.get('/Staff/:id', getStaffById);
 
-router.get('/getCovidData/:date', numberCasesToday);
+router.patch('/Report/Positive/:aadharId', ReportCovidCase);
+router.patch('/Report/Negative/:aadharId', ReportCovidNegative);
+router.patch('/Report/Death/:aadharId', ReportDeath);
+
+router.get('/CovidData/Daily/:date', numberCasesToday);
+router.get('/CovidData/Daily', totalDailyData);
+router.get('/CovidData/Cummilative/:date', covidDataCummilative);
+router.get('/CovidData/Cummilative', totalCovidDataCummilative);
+
+router.post('/Create/User', createUser);
+router.post('/Create/Hospital', createHospital);
+router.post('/Create/Admin', createAdmin);
+router.post('/Create/Staff', createStaff);
+
+router.patch('Hospital/Add/:id', addVacccine);
+
+router.post('/UpdateCummilative', updateCummilativeData);
+
 export default router;
