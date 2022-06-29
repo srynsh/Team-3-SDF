a701c7c692284b7caece082d24344543e4c863c9
Suryaansh-Jain <91973466+Suryaansh-Jain@users.noreply.github.com>
2022-06-29 16:04:03 +0530

    Update index.js

--- new_js_rest_cp/routes/index.js
+++ new_js_rest_cp/routes/index.js
@@ -1,61 +1,74 @@
 import express from "express";
 import {
-    getAllUser,
-    getUserById,
-    getAllPositiveUsers,
-    getAllAdmin,
-    getAdminById,
-    getStaffById,
-    getAllStaff,
-    getAllHospital,
-    getHospitalById,
-    getHospitalWithVaccine,
-    ReportCovidCase,
-    numberCasesToday,
-    ReportCovidNegative,
-    covidDataCummilative,
-    createAdmin,
-    createHospital,
-    createUser,
-    createStaff,
-    addVacccine,
-    totalDailyData,
-    totalCovidDataCummilative,
-    ReportDeath,
-    updateCummilativeData
+  getAllUsers,
+  getUserById,
+  getAllPositiveUsers,
+  getAllAdmin,
+  getAdminById,
+  getStaffById,
+  getAllStaff,
+  getAllHospital,
+  getHospitalById,
+  getHospitalWithVaccine,
+  numberCasesToday,
+  covidDataCummilative,
+  createAdmin,
+  createHospital,
+  createUser,
+  createStaff,
+  totalDailyData,
+  totalCovidDataCummilative,
+  ReportDeath,
+  updateCummilativeData,
+  reportCovidStatus,
+  covidDataCummilativeRange,
+  numberCasesRange,
+  getHospitalWithBooking,
+  restockVaccine,
+  vaccinateUser,
+  newAppointment,
 } from "../controllers/Products.js";
 const router = express.Router();
 
-router.get('/User', getAllUser);
-router.get('/User/Positive', getAllPositiveUsers);
-router.get('/User/:aadharId', getUserById);
+router.get("/User", getAllUsers);
+router.get("/User/Positive/:IsPositive", getAllPositiveUsers);
+router.get("/User/:aadharId", getUserById);
 
-router.get('/Admin/:id', getAdminById);
-router.get('/Admin', getAllAdmin);
+router.get("/Admin/:id", getAdminById);
+router.get("/Admin", getAllAdmin);
 
-router.get('/Hospital', getAllHospital);
-router.get('/Hospital/WithVaccine', getHospitalWithVaccine);
-router.get('/Hospital/:id', getHospitalById);
+router.get("/Hospital", getAllHospital);
+router.get("/Hospital/Vaccine/:VaccineAvailability", getHospitalWithVaccine);
+router.get("/Hospital/:id", getHospitalById);
+router.get("/Hospital/Booking/:BookingAvailable", getHospitalWithBooking);
 
-router.get('/Staff', getAllStaff);
-router.get('/Staff/:id', getStaffById);
+router.get("/Staff", getAllStaff);
+router.get("/Staff/:id", getStaffById);
 
-router.patch('/Report/Positive/:aadharId', ReportCovidCase);
-router.patch('/Report/Negative/:aadharId', ReportCovidNegative);
-router.patch('/Report/Death/:aadharId', ReportDeath);
+router.patch("/Report/Death/:aadharId", ReportDeath);
+router.patch("/User/:aadharId/:CovidStatus", reportCovidStatus);
 
-router.get('/CovidData/Daily/:date', numberCasesToday);
-router.get('/CovidData/Daily', totalDailyData);
-router.get('/CovidData/Cummilative/:date', covidDataCummilative);
-router.get('/CovidData/Cummilative', totalCovidDataCummilative);
+router.get("/CovidData/Daily/:date", numberCasesToday);
+router.get("/CovidData/Daily/:dateStart/:dateEnd", numberCasesRange);
+router.get("/CovidData/Daily", totalDailyData);
+router.get("/CovidData/Cummilative/:date", covidDataCummilative);
+router.get(
+  "/CovidData/Cummilative/:dateStart/:dateEnd",
+  covidDataCummilativeRange
+);
+router.get("/CovidData/Cummilative", totalCovidDataCummilative);
 
-router.post('/Create/User', createUser);
-router.post('/Create/Hospital', createHospital);
-router.post('/Create/Admin', createAdmin);
-router.post('/Create/Staff', createStaff);
+router.post("/Create/User", createUser);
+router.post("/Create/Hospital", createHospital);
+router.post("/Create/Admin", createAdmin);
+router.post("/Create/Staff", createStaff);
 
-router.patch('Hospital/Add/:id', addVacccine);
+router.post("/UpdateCummilative", updateCummilativeData);
 
-router.post('/UpdateCummilative', updateCummilativeData);
+router.patch("/Hospital/Restock/:hospitalID", restockVaccine);
+
+router.post("/Hospital/NewAppointment/:hospitalID", newAppointment);
+
+router.post("/Vaccinate/:hospitalID/:aadharId/:appoinmentId", vaccinateUser);
 
 export default router;
