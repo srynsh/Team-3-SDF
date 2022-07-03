4630451d8a046aa50bbc92b4c0afb24164fc717a
Suryaansh Jain <91973466+srynsh@users.noreply.github.com>
2022-07-03 23:22:50 +0530

    Update index.js

--- new_js_rest_cp/index.js
+++ new_js_rest_cp/index.js
@@ -1,28 +1,48 @@
-import db from './conn.js';
+import db from "./conn.js";
 import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/index.js";
 import cron from "node-cron";
-import { updateCummilativeDataInternal } from "./controllers/Products.js";
+import {
+  updateCummilativeDataInternal,
+  createDay,
+} from "./controllers/Products.js";
 const app = express();
 
 try {
-    await db.authenticate();
-    console.log('Database connected...');
+  await db.authenticate();
+  console.log("Database connected...");
 } catch (error) {
-    console.error('Connection error:', error);
+  console.error("Connection error:", error);
 }
 
 app.use(cors());
 app.use(express.json());
-app.use('/', productRoutes);
+app.use("/", productRoutes);
 
-// cron.schedule("*/10 * * * * *", () => {
-//     let prevDate = new Date();
-//     prevDate.setDate(prevDate.getDate() - 1);
-//     let prevDateStr = prevDate.toLocaleString("fr-CA", { timeZone: 'Asia/Kolkata' }).split(',')[0];//toISOString().split('T')[0];
-//     console.log(prevDateStr);
-//     updateCummilativeDataInternal(prevDateStr);
-// });
+cron.schedule("0 0 1 * * *", () => {
+  let prevDate = new Date();
+  prevDate.setDate(prevDate.getDate() - 1);
+  let prevDateStr = prevDate
+    .toLocaleString("fr-CA", { timeZone: "Asia/Kolkata" })
+    .split(",")[0]; //toISOString().split('T')[0];
+  console.log(prevDateStr);
+  updateCummilativeDataInternal(prevDateStr);
+});
 
-app.listen(5002, () => { console.log('Running on PORT 5002') });
+cron.schedule("0 0 22 * * *", () => {
+  let nextDate = new Date();
+  nextDate.setDate(nextDate.getDate() + 1);
+  let nextDateStr = nextDate
+    .toLocaleString("fr-CA", { timeZone: "Asia/Kolkata" })
+    .split(",")[0]; //toISOString().split('T')[0];
+  console.log(nextDateStr);
+  createDay(nextDateStr);
+});
+
+//cron.schedule("0 0 22 * * *", updateCummilativeDataInternal);
+cron.schedule("0 0 1 * * *", () => {});
+
+app.listen(5002, () => {
+  console.log("Running on PORT 5002");
+});
