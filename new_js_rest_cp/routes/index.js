3777de36f5a9eca75207285cbf53e09d7c235e81
Suryaansh Jain <91973466+srynsh@users.noreply.github.com>
2022-07-09 16:30:08 +0530

    Update index.js

--- new_js_rest_cp/routes/index.js
+++ new_js_rest_cp/routes/index.js
@@ -30,18 +30,25 @@ import {
   authenticateAdmin,
   authenticateStaff,
   aunthicateUser,
+  getAllBookings,
+  createUser2,
+  ReportCovidCase,
+  ReportCovidNegative,
+  getCovidDataWithFilters,
 } from "../controllers/Products.js";
 const router = express.Router();
 
-//Gets all the users. Needs no input
-//Call: 192.168.51.73:5002/User
+// Gets all the users. Needs no input
+// Call: 192.168.51.73:5002/User
+// To get all male users with covid positive and who have taken 2 doses
+// Call: 192.168.51.73:5002/User?isCovidPositive=1&gender=M&numberOfVaccines=2
 router.get("/User", getAllUsers);
 
 //Call: 192.168.51.73:5002/User/Positive/IsPositive
-router.get("/User/Positive/IsPositive", getAllPositiveUsers);
+router.get("/User/Positive/IsPositive", getAllPositiveUsers); //To be deprecated
 
 //Gets the user with the given userId.
-//Sample Call: 192.168.51.73:5002/User/1
+//Sample Call: 192.168.51.73:5002/User/A123
 //Will return the user with id 1
 router.get("/User/:aadharId", getUserById);
 
@@ -58,14 +65,14 @@ router.get("/Hospital", getAllHospital);
 
 //Gets all the hospitals with vaccine.
 //Call: 192.168.51.73:5002/Hospital/Vaccine/1
-router.get("/Hospital/Vaccine/:VaccineAvailability", getHospitalWithVaccine);
+router.get("/Hospital/Vaccine/:VaccineAvailability", getHospitalWithVaccine); // To be deprecated.
 
 //Gets the hospital with the given id.
 //Sample Call: 192.168.51.73:5002/Hospital/1
 //Gives the hospital with ID=1.
 router.get("/Hospital/:id", getHospitalById);
 
-//Gets all the hospitals where people can still book an aapointment.
+//Gets all the hospitals where people can still book an appointment.
 //Call: 192.168.51.73:5002/Hospital/Booking/1
 router.get("/Hospital/Booking/:BookingAvailable", getHospitalWithBooking);
 
@@ -78,7 +85,9 @@ router.get("/Staff/:id", getStaffById);
 //Used to report a death
 //Sample call: 192.168.51.73:5002/Report/Death/A123
 //Will report user with aadharId=A123 dead.
-router.patch("/Report/Death/:aadharId", ReportDeath);
+router.patch("/User/ReportDeath/:aadharId", ReportDeath);
+router.patch("/User/ReportCovidPositive/:aadharId", ReportCovidCase);
+router.patch("/User/ReportCovidNegative/:aadharId", ReportCovidNegative);
 
 //Used to report covid -ve and covid +ve
 //Sample call: 192.168.51.73:5002/User/Report/A123/1
@@ -87,8 +96,18 @@ router.patch("/Report/Death/:aadharId", ReportDeath);
 //Sample call: 192.168.51.73:5002/User/Report/A123/0
 //Body has this {"date": "2022-07-03"}
 //This will report aadharId=A123 as covid -ve on the date of 2022-07-03 (YYYY-MM-DD)
-router.patch("/User/Report/:aadharId/:CovidStatus", reportCovidStatus);
+router.patch("/User/Report/:aadharId/:CovidStatus", reportCovidStatus); //To be deprecated
 
+//Sample Call(for no gender filter): http://192.168.51.73:5002/CovidData?startAge=18&endAge=45&NumberOfVaccinesStart=0&NumberOfVaccinesEnd=60&date=2022-07-09
+//Sample Call(for gender filter (Searching in F)): http://192.168.51.73:5002/CovidData?startAge=18&endAge=45&NumberOfVaccinesStart=0&NumberOfVaccinesEnd=60&date=2022-07-09&gender=F
+//This will give
+//{
+//   "numberOfDeath": <Some number>,
+//   "numberOfRecovered": <Some number>,
+//   "numberOfReported": <Some number>,
+//   "numberOfActive": <Some number>
+// }
+router.get("/CovidData", getCovidDataWithFilters);
 router.get("/CovidData/Daily/:date", numberCasesToday);
 router.get("/CovidData/Daily/:dateStart/:dateEnd", numberCasesRange);
 router.get("/CovidData/Daily", totalDailyData);
@@ -101,33 +120,36 @@ router.get("/CovidData/Cummilative", totalCovidDataCummilative);
 
 //Sample Call: 192.168.51.73:5002/Create/User
 //Body has {"aadharId": "A123", "age": "18", "name": "Suryaansh Jain", "mobileNumber": "9160003106", "gender": "M", "numberOfVaccines": "2", "deathStatus": "0", "isCovidPositive": "0", "password": "ALPHA#!"}
-router.post("/Create/User", createUser);
+router.put("/Create/User", createUser); //To be deprecated
+
+router.put("/User/:aadharId", createUser2);
 
 //Sample Call: 192.168.51.73:5002/Create/Hospital
 //Body has {"hospitalName": "Care", "numberOfVaccines": "2000", "numberOfBookings": "2000" }
-router.post("/Create/Hospital", createHospital);
+router.post("/Hospital", createHospital);
 
-router.post("/Create/Admin", createAdmin);
-router.post("/Create/Staff", createStaff);
+router.post("/Admin", createAdmin);
+router.post("/Staff", createStaff);
 
-router.post("/UpdateCummilative", updateCummilativeData);
+router.post("/UpdateCummilative", updateCummilativeData); //To be deprecated
 
 //Sample Call: 192.168.51.73:5002/Hospital/Restock/1
 //Body has {"count": "2000"}
 router.patch("/Hospital/Restock/:hospitalID", restockVaccine);
 
-//Sample Call: 192.168.51.73:5002/Hospital/NewAppointment/1
-//Body has {"aadharId": "A123", "appoinmentId": "1", "bookingDate": "2022-06-30", "hospitalID": "1"}
-router.post("/Hospital/NewAppointment/:hospitalID", newAppointment);
+//Sample Call: 192.168.51.73:5002/Hospital/NewAppointment/1/3
+//Body has {"aadharId": "A123", "bookingDate": "2022-06-30"}
+router.put("/Hospital/Appointment/:hospitalID/:appoinmentId", newAppointment);
 
-//Sample Call: 192.168.51.73:5002/Vaccinate/1/A123/1
-//Body has {"aadharId": "A123", "appoinmentId": "1", "hospitalID": "1", "bookingDate": "2022-06-30"}
-router.post("/Vaccinate/:hospitalID/:aadharId/:appoinmentId", vaccinateUser);
+//Sample Call: 192.168.51.73:5002/Appointment/Complete/2
+router.put("/Appointment/Complete/:appoinmentId", vaccinateUser);
 
-router.get("/Authenticate/User", aunthicateUser);
+router.post("/Authenticate/User", aunthicateUser);
 
-router.get("/Authenticate/Admin", authenticateAdmin);
+router.post("/Authenticate/Admin", authenticateAdmin);
 
-router.get("/Authenticate/Staff", authenticateStaff);
+router.post("/Authenticate/Staff", authenticateStaff);
+
+router.get("/Hospital/AllBooking/:hospitalID", getAllBookings);
 
 export default router;
